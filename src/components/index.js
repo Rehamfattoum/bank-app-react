9d22792d5113f62e9d2f33094649b80b07ed2e9d
Rehamfattoum <rehamfatoum777@gmail.com>
2024-03-23 18:37:46 +0300

    'my-bank-react-app'

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from "./billing";
+import Business from "./business";
+import CardDeal from "./cardDeal";
+import Clients from "./clients";
+import CTA from "./CTA";
+import Footer from './footer';
+import Hero from './hero';
+import NavBar from './navBar';
+import Stats from './stats';
+import Testimonials from './testimonials';
+
+export{
+Billing ,
+Business,
+CardDeal,
+Clients,
+CTA,
+Footer,
+Hero,
+NavBar,
+Stats,
+Testimonials,
+}
